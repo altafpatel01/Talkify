1856e3f12bc43a6967fbafeddf410dded9d87145
Altaf <altafpatel8485@gmail.com>
2024-12-24 01:37:10 +0530

    create the layout of the login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,20 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-
+import './index.css';
+import SignUp from './pages/Signup/Signup.jsx';
+import Login from './pages/Login/Login.jsx';
+import { Navigate, Route, Routes } from "react-router-dom";
 function App() {
  
 
   return (
     <>
-    <button className="btn">Button</button>
+   
+    <div className='p-4 h-screen flex items-center justify-center'>
+		<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={ <SignUp />} />
+		</div>
     </>
   )
 }
